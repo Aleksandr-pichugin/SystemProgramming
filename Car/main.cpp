af8b765ddfde473a3be4694a963776392ef776a9
Александр Пичугин <aleksandrpicugin700@gmail.com>
2025-05-15 00:25:31 +0300

    Engine DONE

--- Car/main.cpp
+++ Car/main.cpp
@@ -58,9 +58,62 @@ public:
 	}
 };
 
+#define MIN_ENGINE_CONSAPTION  3
+#define MAX_ENGINE_CONSAPTION  25
+
+class Engine
+{
+	const double CONSUMPTION; // Расход на 100 км
+	const double DEFAULT_CONSUPTION_PER_SECOND;
+	double consumption_per_second;
+	bool is_started;
+public:
+	double get_consuption_per_second()const
+	{
+		return consumption_per_second;
+	}
+	Engine(double consuption):
+		CONSUMPTION
+	(
+		consuption<MIN_ENGINE_CONSAPTION ? MIN_ENGINE_CONSAPTION :
+		consuption > MAX_ENGINE_CONSAPTION ? MAX_ENGINE_CONSAPTION:
+		consuption
+	),
+		DEFAULT_CONSUPTION_PER_SECOND(CONSUMPTION*3e-5),
+		consumption_per_second(DEFAULT_CONSUPTION_PER_SECOND)
+	{
+		cout << "Engine is ready" << endl;
+	}
+	~Engine()
+	{
+		cout << "Engine is over" << endl;
+	}
+	void start()
+	{
+		is_started = true;
+	}
+	void stop()
+	{
+		is_started = false;
+	}
+	bool started()const
+	{
+		return is_started;
+	}
+	void info()const
+	{
+		cout << "Consumption: " << CONSUMPTION << " liters/100km\n";
+		cout << "Default  Consumption: " << DEFAULT_CONSUPTION_PER_SECOND << " liters/s\n";
+		cout << "Consumption: " << consumption_per_second << " liters/s\n";
+	}
+};
+//#define TANK_CHECK
+//#define ENGINE_CHECK
+
 void main()
 {
 	setlocale(LC_ALL, "");
+#ifdef TANK_CHECK
 	Tank tank(80);
 	double fuel;
 	do
@@ -69,8 +122,11 @@ void main()
 		tank.fill(fuel);
 		tank.info();
 	} while (true);
-	{
+#endif // TANK_CHECK
 
-	}
-	tank.info();
+#ifdef ENGINE_CHECK
+	Engine engine(10);
+	engine.info();
+#endif // ENGINE_CHECK
+
 }
