d15aa3ef6af2eea86e8bec9e8f06f03586bb6403
Александр Пичугин <aleksandrpicugin700@gmail.com>
2025-05-19 22:58:25 +0300

    engine_idle DONE

--- Car/main.cpp
+++ Car/main.cpp
@@ -2,9 +2,11 @@
 #include<iostream>
 #include<conio.h>
 #include<thread>
+#include<chrono>
 using std::cin;
 using std::cout;
 using std::endl;
+using namespace std::chrono_literals;
 
 #define Enter   13
 #define Escape  27
@@ -128,6 +130,7 @@ class Car
 	struct
 	{
 		std::thread panel_thread;
+		std::thread engine_idle_thread;
 	}threads_container; // Эта структура не имеет имени и реализует только один экземпляр.
 public:
 	Car(double consumption, int capacity, int max_speed=250):
@@ -161,12 +164,27 @@ public:
 		system("CLS");
 		cout << "Yor are out of the Car" << endl;
 	}
+	void start()
+	{
+		if (tank.get_fuel_level())
+		{
+			engine.start();
+			threads_container.engine_idle_thread = std::thread(&Car::engine_idle, this);
+		}
+	}
+	void stop()
+	{
+		engine.stop();
+		if (threads_container.engine_idle_thread.joinable())
+			threads_container.engine_idle_thread.join();
+	}
 	void control()
 	{
 		char key = 0;
 		do
 		{
-			key = _getch();
+			key = 0;
+			if(_kbhit())key = _getch();
 			switch (key)
 			{
 			case Enter:
@@ -176,18 +194,38 @@ public:
 				double fuel;
 				cout << "Введите обьем топлива: "; cin >> fuel;
 				tank.fill(fuel);
+				break;
+			case'I':case'i': //Ignition
+				if (driver_inside)!engine.started() ? start() : stop();
+				break;
 			case Escape:
+				stop();
 				get_out();
 			}
+		if (tank.get_fuel_level() <= 0)stop();
 		} while (key != Escape);
 		// Concurent execution (одновременное выполнение).
+	}
+	void engine_idle()
+	{
+		while (engine.started() && tank.give_fuel(engine.get_consuption_per_second()))
+		{
+			std::this_thread::sleep_for(1s);
+		}
 	}
 	void panel()
 	{
 		while (driver_inside)
 		{
 			system("CLS");
-			cout << "Fuel level: " << tank.get_fuel_level() << " liters\n";
+			cout << "Fuel level: " << tank.get_fuel_level() << " liters";
+			if (tank.get_fuel_level() < 5)
+			{
+				HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+				SetConsoleTextAttribute(hConsole, 0xCF);
+					cout<<" LOW FUEL ";
+					SetConsoleTextAttribute(hConsole, 0x07);
+			}
 			cout << "Engine is " << (engine.started() ? "started" : "stoped") << endl;
 			cout << "Speed:\t" << speed << " km/h\n";
 			Sleep(100);
